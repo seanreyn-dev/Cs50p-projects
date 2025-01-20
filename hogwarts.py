ba7d82b5097d64321d645a57a097f5b5a2eb51cf
seanreyn-dev <reynolds.sean01@gmail.com>
2025-01-20 13:44:04 -0800

    Add files via upload

---
+++ hogwarts.py
@@ -0,0 +1,10 @@
+students = [
+    {"name": "Hermione", "house": "Gryffindor", "patronus": "Otter"},
+    {"name": "Harry", "house": "Gryffindor", "patronus": "Otter"},
+    {"name": "Ron", "house": "Gryffindor", "patronus": "Jack Russell Terrier"},
+    {"name": "Draco", "house": "Slytherin", "patronus": None},
+]
+
+
+for student in students:
+    print(student["name"], student["house"], student["patronus"], sep=", ")
